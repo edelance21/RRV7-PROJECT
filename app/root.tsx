3159ed558bc7f4d2bd593a53a8567181d6a22197
Delance <edelance@hotmail.es>
2026-07-17 18:10:13 -0400

    rutas y la maquetacion del home definida

--- app/root.tsx
+++ app/root.tsx
@@ -45,8 +45,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
     <div>
-      <Outlet/>
       <Navbar/>
+      <Outlet/>
     </div>
   );
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,59 @@
+import { Link } from "react-router";
 import type { Route } from "./+types/home";
 
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "RESTExplorer" },
+    { name: "Check out country data!", content: "Welcome to RESTExplorer" },
   ];
 }
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className="px-2 py-32 bg-white md:px-0">
+      <div className="container items-center max-w-6xl mx-auto xl:px-5">
+        <div>
+          <div>
+            <div>
+              <h1>
+                <span>Explore Countries with</span>
+                <span>Real-Time Data</span>
+              </h1>
+
+              <p>
+                Discover details about every country 
+                around the world - from capitals to regions!
+              </p>
+              <div>
+                <Link to="/countries"> 
+                  Explorer Now
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-5 h-5 ml-1"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                    <polyline points="12 5 19 12 12 19"></polyline>
+                  </svg> 
+                </Link>
+
+                <Link to="/about"> Learn More </Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img src="https://image.winudf.com/v2/image/Ymx1ZWNhcC5pbWFnZXouYmVhdXRpZnVsX2NvdW50cmllc193YWxscGFwZXJzX3NjcmVlbl8wXzlpMW14OWl1/screen-0.webp?fakeurl=1&type=.webp" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
